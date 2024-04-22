7dc53c9b8fe74cb9ec872191b84bf8896a208b1d
Artur Reiljan <arreil@taltech.ee>
2024-04-22 14:58:36 +0300

    Ready to merge, hud now takes correct currentTime. Zones spawn at exactly the right coordinates.

--- core/src/ee/taltech/gandalf/network/NetworkClient.java
+++ core/src/ee/taltech/gandalf/network/NetworkClient.java
@@ -92,7 +92,7 @@ public class NetworkClient {
     public void connect() {
         // Connect client with the server.
         try {
-            client.connect(5000, "localhost", 8080, 8081);
+            client.connect(5000, "193.40.255.34", 8080, 8081);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
